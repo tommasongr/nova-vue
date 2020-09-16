0bac18c83bef690815519e60651c2941bf6259ac
Tommaso Negri <tommasongr@icloud.com>
2020-09-16 20:48:50 +0000

    FEATURE: Add correctly support for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
     root: true,
     env: {
+        es6: true,
+        node: true,
         'nova/nova': true,
     },
-    plugins: ['nova'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'nova'],
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-import commonjs from '@rollup/plugin-commonjs'
-// const nodeResolve = require('@rollup/plugin-node-resolve')
+import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 module.exports = {
     input: 'src/Scripts/main.js',
 
-    plugins: [commonjs(), nodeResolve()],
+    plugins: [typescript(), nodeResolve()],
 
     output: {
         file: 'Vue.novaextension/Scripts/main.dist.js',
         format: 'cjs',
         exports: 'auto',
+        sourcemap: true,
     },
 }
