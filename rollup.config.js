bd3060b708dd431112d62cae5c5bc00f7b949702
Tommaso Negri <tommasongr@icloud.com>
2020-09-21 22:28:54 +0000

    FEATURE: Add first set of VSCode snippets

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 // const nodeResolve = require('@rollup/plugin-node-resolve')
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 module.exports = {
     input: 'src/Scripts/main.js',
 
-    plugins: [commonjs(), nodeResolve()],
+    plugins: [commonjs(), json(), nodeResolve()],
 
     output: {
         file: 'Vue.novaextension/Scripts/main.dist.js',
--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -1,1 +1,13 @@
-console.log('Vue Online')
+import './registrars/vueRegistrar.js'
+// require('./registrars/vueTemplateRegistrar.js');
+// require('./registrars/vueScriptRegistrar.js');
+
+export function activate() {
+    // Do work when the extension is activated
+    console.log('Hello from Vue!')
+}
+
+export function deactivate() {
+    // Clean up state before the extension is deactivated
+    console.log('Goodbye!')
+}
