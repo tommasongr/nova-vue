12faff90442e8d82d0529f598ef20b3859bad413
Tommaso Negri <tommasongr@icloud.com>
2020-11-25 07:47:13 +0000

    FEATURE: Add support for PUG Syntax

---
+++ Vue.novaextension/Tests/ComponentPug.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    .container
+      .row
+        .col-sm-3(v-for="item in items" v-bind:key="item.id")
+          .artist
+            img.artist__image(v-bind:src="item.image")
+            h4.text-center {{ item.title }}
+</template>
+
+<script>
+import SurveyInput from './SurveyInput.vue'
+export default {
+    extends: SurveyInput,
+    props: {
+        placeholder: String,
+    },
+}
+</script>
