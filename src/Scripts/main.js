cf16751f05c99e2800c1cf51945223c81bf4d890
Tommaso Negri <tommasongr@icloud.com>
2021-04-30 16:30:56 +0000

    feat: clean up code and add Vue Sidebar

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -1,15 +1,14 @@
-import ensureInstallation from './install'
-import { showNotification, wrapCommand } from './helpers'
-import isVeturModeEnabled from './isVeturModeEnabled'
+import { InformationView } from './informationView'
+import { showNotification, getVlsVersion } from './helpers'
 import { dependencyManagement } from 'nova-extension-utils'
 import { VueLanguageServer } from './VueLanguageServer'
 
 // Register a Nova command for opening project preferences
 nova.commands.register(
     'tommasonegri.vue.commands.openWorkspaceConfig',
-    wrapCommand(function openWorkspaceConfig(workspace) {
+    function openWorkspaceConfig(workspace) {
         workspace.openConfig()
-    })
+    }
 )
 
 // Register a Nova command for reloading the extension
@@ -23,38 +22,148 @@ dependencyManagement.registerDependencyUnlockCommand(
 let langserver = null
 
 // Initialize a new Nova Disposable object
-// Read more: https://docs.nova.app/api-reference/disposable/
 const compositeDisposable = new CompositeDisposable()
 
 async function asyncActivate() {
+    // Instantiate the Information Sidebar
+    const informationView = new InformationView()
+    compositeDisposable.add(informationView)
+
+    informationView.status = 'Activating...'
+
+    // If VLS isn't found, install it
     try {
-        await ensureInstallation(compositeDisposable)
-
-        langserver = new VueLanguageServer()
-
-        console.log('Hello from Vue!')
-    } catch (err) {
-        console.error('Unable to set up Vue service', err, err.stack)
-
-        if (err.status === 127) {
-            return showNotification(
-                'vue-resolution-error',
-                `Can't find npm and Vue`,
-                `Vue couldn't be found because npm isn't available. Please make sure you have Node installed. If you've only installed Node through NVM, you'll need to change your shell configuration to work with Nova. See https://library.panic.com/nova/environment-variables/`
-            )
-        }
-
-        return showNotification(
-            'vue-resolution-error',
-            `Unable to start Vue`,
-            `Please check the extension console for additional logs.`
+        await dependencyManagement.installWrappedDependencies(
+            compositeDisposable,
+            {
+                console: {
+                    log: (...args) => {
+                        if (nova.inDevMode()) {
+                            console.log('dependencyManagement:', ...args)
+                        }
+                    },
+                    info: (...args) => {
+                        if (nova.inDevMode()) {
+                            console.info('dependencyManagement:', ...args)
+                        }
+                    },
+                    warn: (...args) => {
+                        console.warn('dependencyManagement:', ...args)
+                    },
+                },
+            }
         )
+    } catch (err) {
+        informationView.status = 'Failed to install'
+        throw err
     }
+
+    // Retrieve the VLS path
+    const vlsPath = nova.path.join(
+        dependencyManagement.getDependencyDirectory(),
+        'node_modules',
+        '.bin',
+        'vls'
+    )
+
+    // If VLS hasn't been installed, display an error message and quit
+    if (!vlsPath) {
+        informationView.status = 'VLS not found'
+
+        showNotification(
+            'vls-not-found',
+            'VLS Not Found',
+            "The Vue Language Server hasn't been installed correctly. You may want to restart the server or open an issue. I'm sorry for this...",
+            ['Restart', 'Open Issue', 'Ignore'],
+            (reply) => {
+                console.log(reply.actionIdx)
+                switch (reply.actionIdx) {
+                    case 0:
+                        nova.commands.invoke('tommasonegri.vue.commands.reload')
+                        break
+                    case 1:
+                        nova.openURL(
+                            'https://github.com/tommasongr/nova-vue/issues'
+                        )
+                        break
+                    case 2:
+                        break
+                }
+            }
+        )
+
+        return
+    } else {
+        if (nova.inDevMode()) {
+            console.info('Using VLS at:', vlsPath)
+        }
+    }
+
+    // Instantiate the Vue Language Server
+    langserver = new VueLanguageServer()
+
+    // Add an event listener for when Vue Language Server stops
+    compositeDisposable.add(
+        langserver.languageClient.onDidStop((err) => {
+            informationView.status = 'Stopped'
+
+            // Display an error message and a restart button if the server stopped unexpectedly
+            if (err) {
+                let message = 'Vue Language Server stopped unexpectedly'
+                message += `:\n\n${err.toString()}`
+                message +=
+                    '\n\nPlease report this, along with any output in the Extension Console.'
+
+                nova.workspace.showActionPanel(
+                    message,
+                    {
+                        buttons: ['Restart', 'Ignore'],
+                    },
+                    (index) => {
+                        if (index == 0) {
+                            nova.commands.invoke(
+                                'tommasonegri.vue.commands.reload'
+                            )
+                        }
+                    }
+                )
+            }
+        })
+    )
+
+    // Retrieve the running VLS version to display in the Information Sidebar
+    getVlsVersion()
+        .then((version) => {
+            console.log('VLS Version', version)
+            informationView.vlsVersion = version
+        })
+        .catch((err) => {
+            console.log('No VLS Version:', err)
+        })
+
+    informationView.status = 'Running'
 }
 
-exports.activate = function () {
-    // Do work when the extension is activated
+export function activate() {
+    console.log('Activating Vue Language Server...')
+
+    if (nova.inDevMode()) {
+        showNotification(
+            'vue-activated',
+            'Vue is Loading',
+            'Vue extension si loading'
+        )
+    }
+
     return asyncActivate()
+        .then(() => {
+            console.log('Hello from Vue 💎')
+        })
+        .catch((err) => {
+            console.error('Failed to activate')
+            console.error(err)
+            nova.workspace.showErrorMessage(err)
+        })
 }
 
 export function deactivate() {
@@ -66,11 +175,18 @@ export function deactivate() {
 
     compositeDisposable.dispose()
 
-    console.log('Goodbye from Vue!')
+    console.log('Goodbye from Vue 👋')
 }
 
 async function reload() {
     deactivate()
-    console.log('Reloading Vue Server...')
+
+    console.log('Reloading Vue Language Server...')
+    showNotification(
+        'vue-reload',
+        'Vue is Reloading...',
+        "Don't worry, it won't take a while."
+    )
+
     await asyncActivate()
 }
