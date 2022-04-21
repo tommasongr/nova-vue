63db0778e1a7fb84524e26a9de93c2f61499dbf3
Tommaso Negri <tommaso@hey.com>
2022-04-21 22:44:07 +0200

    fix: Detect correctly alternatives languages in style and script tags

--- Vue.novaextension/Tests/Small.vue
+++ Vue.novaextension/Tests/Small.vue
@@ -1,21 +1,29 @@
 <template>
-    <input id="ciao" class="ciao" v-for="" />
-    <p v-for="ciao">{{ greeting }} World!</p>
+  <input id="ciao" class="ciao" v-for="" />
+  <p v-for="ciao">{{ greeting }} World!</p>
 </template>
 
 <script>
 module.exports = {
-    data: function () {
-        return {
-            greeting: 'Hello',
-        }
-    },
+  data: function () {
+    return {
+      greeting: 'Hello',
+    }
+  },
 }
 </script>
 
+<style lang="postcss" scoped>
+  .this {
+    .failsToHighlight {
+      display: block;
+    }
+  }
+</style>
+
 <style scoped>
-p {
+  p {
     font-size: 2em;
     text-align: center;
-}
+  }
 </style>
